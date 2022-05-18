667f67e03ac54ad6f48150e928389810686d5927
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2022-05-18 16:10:47 +0200

    Automatically rename the base URL in services during spin-up.

--- services/start.sh
+++ services/start.sh
@@ -15,6 +15,9 @@ SERVICES="$(find ${SCRIPT_DIR} -maxdepth 1 -mindepth 1 -type d)"
 
 for SERVICE in ${SERVICES}
 do
+    echo "Renaming server in all config files for service in ${SERVICE}/ ..."
+    find "${SERVICE}" -type f -exec sed -i "s/apitestbed.geonovum.nl/`hostname -f`/g" {} \; 2> /dev/null
+    echo "Server renaming successfully done!"
 	pushd ${SERVICE}
 		./start.sh
 	popd
